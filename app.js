ca2d446c8d63e6b0fd1b3f502c91600d9ce16ec7
QuackityQuackQuackQuack <auraninja8@gmail.com>
2025-07-27 18:09:31 +0100

    second commit

--- app.js
+++ app.js
@@ -3,17 +3,48 @@ let num2 = document.querySelector("#num2");
 let result = document.querySelector("#result")
 
 function add(){
-    result.innerHTML = Number(num1.value) + Number(num2.value)
+    let add = Number(num1.value) + Number(num2.value);
+    result.innerHTML = add;
 }
 
 function sub(){
-    result.innerHTML = Number(num1.value) - Number(num2.value)
+    let sub = Number(num1.value) - Number(num2.value);
+    result.innerHTML = sub;
 }
 
 function div(){
-     result.innerHTML = Number(num1.value) / Number(num2.value)
+    let div = Number(num1.value) / Number(num2.value);
+    result.innerHTML = div;
 }
 
 function mult(){
-     result.innerHTML = Number(num1.value) * Number(num2.value)
+    let mult = Number(num1.value) * Number(num2.value);
+    result.innerHTML = mult;
+}
+
+function peri(){
+    let peri = (Number(num1.value) + Number(num2.value)) *2;
+    result.innerHTML = peri;
+}
+
+function area(){
+    let area = Number(num1.value) * Number(num2.value);
+    result.innerHTML = area;
+}
+
+function temp(){
+    let inputValue = Number(document.querySelector("#value").value);
+    let cel = document.querySelector("#celsius");
+    let fa = document.querySelector("#farenheit");
+
+    if(cel.checked ) {
+        let celsiusCon = (inputValue * 9)/5 + 32};
+        result.innerHTML = celsiusCon;
+    }
+
+    if(fa.checked ) {
+        let farenheitCon = ((inputValue - 32)/ 1.8 + 32);
+        result.innerHTML = farenheitCon
+        ;
+    }
 }
